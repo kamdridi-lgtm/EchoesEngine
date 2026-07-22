53b0b5953b89fdb4965506ecaeea449b3100c772
kamdridi-lgtm <kamdridi@hotmail.com>
2026-07-22 00:01:02 -0400

    Fix GCC compatibility for PromptDirector defaults

--- ai_prompt/include/EchoesEngine/ai_prompt/PromptDirector.h
+++ ai_prompt/include/EchoesEngine/ai_prompt/PromptDirector.h
@@ -45,8 +45,10 @@ public:
         float bassBias = 1.f;
     };
 
-    static PromptResult generate_prompt_from_audio(const AudioFeatures& features, Settings settings = {});
-    static StyleDescriptor generate_style(const AudioFeatures& features, Settings settings = {});
+    static PromptResult generate_prompt_from_audio(const AudioFeatures& features);
+    static PromptResult generate_prompt_from_audio(const AudioFeatures& features, Settings settings);
+    static StyleDescriptor generate_style(const AudioFeatures& features);
+    static StyleDescriptor generate_style(const AudioFeatures& features, Settings settings);
 };
 
 } // namespace echoes::ai_prompt
